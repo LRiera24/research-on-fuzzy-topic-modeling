0b575bfdaad4bf10c0ccf9a1bb5b84a0c3b60089
LRiera24 <laura.riera2412@gmail.com>
2023-11-03 10:09:38 -0400

    topic_namig base structure

--- src/topic_naming.py
+++ src/topic_naming.py
@@ -1,10 +1,23 @@
+from nltk.corpus import wordnet
+from nltk.corpus import wordnet_ic 
 
-class topic_naming:
-    def get_topic_keywords():
+class TopicNaming:
+    def __init__(self, topic_keywords, domain):
+        self.topic_keywords = topic_keywords
+        self.domain = domain
+
+    def get_topic_keywords(self, word_topic_distro, min_prob = 0.7):
+        # Return the most significant words for the topic
+        self.topic_keywords = [word for word in word_topic_distro if word.probability >= min_prob] #TODO: ver como funciona esto
+
+    def map_keywords_to_domains(self):
+        # Map keywords to relevant domains
         pass
-    def map_keywords_to_domains():
+
+    def get_lowest_common_domain(self, keyword_domains):
+        # Find the lowest common domain for the topic
         pass
-    def get_lowest_common_domain():
-        pass
-    def assign_name_to_topic():
+
+    def assign_name_to_topic(self):
+        # Assign a name to the topic based on the lowest common domain
         pass
