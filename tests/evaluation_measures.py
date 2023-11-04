ab3eb90b047b76e31ff2813ac5a8ae42c1abfc57
LRiera24 <laura.riera2412@gmail.com>
2023-11-04 18:16:00 -0400

    Update evaluation_measures.py

--- tests/evaluation_measures.py
+++ tests/evaluation_measures.py
@@ -2,71 +2,50 @@ import time
 import resource
 from sklearn.metrics import precision_score, recall_score, f1_score
 
-
 class InformationRetrievalEvaluator:
-    def __init__(self, ranking_results, relevance_labels):
-        # List of ranked documents for each query
-        self.ranking_results = ranking_results
-        # Ground truth relevance labels for each query
-        self.relevance_labels = relevance_labels
+    def __init__(self, queries, relevance_labels, ir_system):
+        self.queries = queries  # List of query strings
+        self.relevance_labels = relevance_labels  # List of true ranked results per query
+       
+        self.ir_system = ir_system  # Information Retrieval system
+        self.ranking_results = []   # List of obtained ranked results per query
 
-    def evaluate_precision(self):
-        # Calculate precision for each query
-        precisions = []
-        for query_results, relevance in zip(self.ranking_results, self.relevance_labels):
-            precision = precision_score(
-                relevance, query_results, average='micro')
-            precisions.append(precision)
-        return precisions
+        self.execution_times = []  # List to store execution times
+        self.memory_usages = []  # List to store memory usages
+        self.precisions = []  # List to store precision values
+        self.recalls = []  # List to store recall values
+        self.f1_scores = []  # List to store F1 scores
 
-    def evaluate_recall(self):
-        # Calculate recall for each query
-        recalls = []
-        for query_results, relevance in zip(self.ranking_results, self.relevance_labels):
-            recall = recall_score(relevance, query_results, average='micro')
-            recalls.append(recall)
-        return recalls
+    def evaluate_ir_system(self):
+        for query in self.queries:
+            execution_time, memory_usage = self.measure_execution(query)
+            precision, recall, f1 = self.evaluate_ranking(self.relevance_labels, self.ranking_results)
+            
+            # Store the measurements in the lists
+            self.execution_times.append(execution_time)
+            self.memory_usages.append(memory_usage)
+            self.precisions.append(precision)
+            self.recalls.append(recall)
+            self.f1_scores.append(f1)
 
-    def evaluate_f1_score(self):
-        # Calculate F1 score for each query
-        f1_scores = []
-        for query_results, relevance in zip(self.ranking_results, self.relevance_labels):
-            f1 = f1_score(relevance, query_results, average='micro')
-            f1_scores.append(f1)
-        return f1_scores
-
-    def evaluate_execution_time(self, function):
-        # Measure execution time of a specific function
-        start_time = time.time()
-        function()
-        end_time = time.time()
-        return end_time - start_time
-
-    def evaluate_memory_usage(self, function):
-        # Measure memory usage of a specific function
+    def measure_execution(self, query):
         start_memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-        function()
+        start_time = time.time()
+        # Call your IR system with the query and get ranking results
+        self.ranking_results = self.ir_system(query)
+        end_time = time.time()
         end_memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-        return end_memory - start_memory
 
+        execution_time = end_time - start_time
+        memory_usage = end_memory - start_memory
 
-# Example usage:
-# Instantiate the evaluator with ranking results and relevance labels
-ranking_results = [[1, 3, 2], [2, 1, 3], [3, 2, 1]]
-relevance_labels = [[1, 0, 1], [0, 1, 1], [1, 1, 0]]
-evaluator = InformationRetrievalEvaluator(ranking_results, relevance_labels)
+        return execution_time, memory_usage
 
-# Evaluate precision, recall, and F1 score for the ranking results
-precisions = evaluator.evaluate_precision()
-recalls = evaluator.evaluate_recall()
-f1_scores = evaluator.evaluate_f1_score()
+    def evaluate_ranking(self, relevance_labels, ranking_results):
+        # Evaluate the ranking results using precision, recall, and F1 score
+        # Replace the relevance_labels with the ground truth relevance labels
+        precision = precision_score(relevance_labels, ranking_results, average='micro')
+        recall = recall_score(relevance_labels, ranking_results, average='micro')
+        f1 = f1_score(relevance_labels, ranking_results, average='micro')
 
-# Evaluate execution time and memory usage of a specific function
-execution_time = evaluator.evaluate_execution_time(lambda: time.sleep(2))
-memory_usage = evaluator.evaluate_memory_usage(lambda: [0] * int(1e7))
-
-print("Precision:", precisions)
-print("Recall:", recalls)
-print("F1 Score:", f1_scores)
-print("Execution Time (s):", execution_time)
-print("Memory Usage (KB):", memory_usage)
+        return precision, recall, f1
