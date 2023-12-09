06d091b9ff01930276f240372fda3f0b7877ef68
LRiera24 <laura.riera2412@gmail.com>
2023-12-09 02:16:54 -0500

    Update auto_incremental_clustering.py

--- src/semantic_preprocessing/auto_incremental_clustering.py
+++ src/semantic_preprocessing/auto_incremental_clustering.py
@@ -10,10 +10,12 @@ import numpy as np
 # Constants for dictionary
 CENTROID = 0
 ELEMENTS = 1
+WORDS = 2
 
 class AutoIncrementalClustering:
-    def __init__(self, word_embeddings, model, min_coherence=0.5):
+    def __init__(self, words, word_embeddings, model, min_coherence=0.6):
         # Initialize the class with provided word embeddings, a model, and a minimum coherence threshold.
+        self.words = words
         self.word_embeddings = word_embeddings
         self.min_coherence = min_coherence
         self.model = model
@@ -22,16 +24,17 @@ class AutoIncrementalClustering:
 
     def clustering(self):
         # Perform clustering on the provided word embeddings.
-        for w_embedding in self.word_embeddings:
+        for index, w_embedding in enumerate(self.word_embeddings):
+            print("!!!!!!!!!!!!!", all(w_embedding) != None)
             if len(self.clusters) == 0:
-                self._assignment(w_embedding, None, None)
+                self._assignment(index, w_embedding, None, None)
                 print("FIRST ELEMENT ADDED")
                 continue
             similarities = self._calculate_similarities(w_embedding)
             print("!!!!!!!!!!!!!!! SIMS DONE")
             coherences = self._evaluate_quality(w_embedding, similarities)
             print("!!!!!!!!!!!!!!! QUALITY DONE")
-            self._assignment(w_embedding, similarities, coherences)
+            self._assignment(index, w_embedding, similarities, coherences)
             print("!!!!!!!!!!!!!!! ASSIGNMENT DONE")
 
     def _calculate_similarities(self, word_embedding):
@@ -40,13 +43,15 @@ class AutoIncrementalClustering:
 
         for index, cluster_data in enumerate(self.clusters.values()):
             cluster_centroid = cluster_data[CENTROID]
+            print("c", cluster_centroid)
             similarity = np.dot(word_embedding, cluster_centroid) / (
                 np.linalg.norm(word_embedding) * np.linalg.norm(cluster_centroid))
+            print("sim", similarity)
             similarities.append((index, similarity))
 
         print(similarities)
         # Calculate harmonic mean of similarities and filter clusters based on a threshold.
-        harmonic_mean = hmean([round(sim, 6) for _, sim in similarities])
+        harmonic_mean = hmean([round(sim, 6) for _, sim in similarities if sim >= 0])
         print(harmonic_mean)
         similarities = [(cluster_num, similarity) for cluster_num,
                         similarity in similarities if similarity >= harmonic_mean]
@@ -65,15 +70,12 @@ class AutoIncrementalClustering:
             # Calculate cosine similarities for pairwise combinations of words in the cluster.
             for pair in itertools.combinations(cluster_elements + [word_embedding], 2):
                 pair = sorted(pair, key=lambda x: x[0])
-                print(pair)
                 key = hashlib.md5(f"{pair[0]}-{pair[1]}".encode()).hexdigest()
                 if key in pairwise_sims.keys():
-                    print('here1')
                     similarity = pairwise_sims[key]
                 else:
                     similarity = np.dot(pair[0], pair[1]) / (
                         np.linalg.norm(pair[0]) * np.linalg.norm(pair[1]))
-                    print('here2')
                     pairwise_sims[key] = similarity
 
             # Calculate coherence for the cluster by averaging the pairwise similarities.
@@ -81,19 +83,22 @@ class AutoIncrementalClustering:
             coherences.append(coherence)
 
         # Normalize coherences between 0 and 1, and filter clusters based on a coherence threshold.
-        min_coherence = max(coherences)
-        if min_coherence > 0:
-            coherences = [coherence /
-                          min_coherence for coherence in coherences]
+        min_coherence = min(coherences)
+        max_coherence = max(coherences)
+
+        if min_coherence < max_coherence:
+            range_coherence = max_coherence - min_coherence
+            coherences = [(coherence - min_coherence) / range_coherence for coherence in coherences]
         else:
             coherences = [0.0] * len(coherences)
 
         coherences = [(cluster_num, coherence) for cluster_num, coherence in zip(
             self.clusters.keys(), coherences) if coherence >= self.min_coherence]
 
+        print(coherences)
         return coherences
 
-    def _assignment(self, word_embedding, similarities, coherences):
+    def _assignment(self, index, word_embedding, similarities, coherences):
         # Assign the word embedding to an existing or new cluster based on similarities and coherences.
         common_clusters = set(index1 for index1, _ in similarities) & set(
             index2 for index2, _ in coherences) if similarities and coherences else []
@@ -102,17 +107,16 @@ class AutoIncrementalClustering:
             print('assignment')
             # If no clusters exist or there are no common clusters, create a new cluster.
             self.clusters[len(self.clusters)] = [
-                word_embedding, [word_embedding]]
-            print(self.clusters)
+                word_embedding, [word_embedding], [self.words[index]]]
         else:
             for cluster_num in common_clusters:
                 # Update an existing cluster with the new word embedding.
-                updated_elements = self.clusters[cluster_num][ELEMENTS].append(
-                    word_embedding)
-                new_centroid = mean(updated_elements)
+                self.clusters[cluster_num][ELEMENTS].append(word_embedding)
+                updated_elements = self.clusters[cluster_num][ELEMENTS]
+                new_centroid = np.mean(updated_elements, axis=0)
                 self.clusters[cluster_num][CENTROID] = new_centroid
                 self.clusters[cluster_num][ELEMENTS] = updated_elements
-
+                self.clusters[cluster_num][WORDS].append(self.words[index])
 
 # Load pre-trained word embeddings (Word2Vec model)
 model_path = os.path.abspath('src')
@@ -129,5 +133,7 @@ context = ['school', 'class', 'homework', 'student', 'book', 'knowledge', 'learn
 
 embeddings = [word2vec_model[word] for word in context]
 
-cluster = AutoIncrementalClustering(embeddings, word2vec_model)
+cluster = AutoIncrementalClustering(context, embeddings, word2vec_model)
 cluster.clustering()
+for t in cluster.clusters.values():
+    print(t[WORDS])
